7c14f83165ae0ce3460c142e51305c7f5d3ab2e8
vishusarkar9810 <vishwajeetsarkar113106@gmail.com>
2025-06-06 00:47:29 +0530

    Initial Commit

---
+++ Budget Planner/Budget_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_PlannerApp.swift
+//  Budget Planner
+//
+//  Created by Vishwajeet Sarkar on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Budget_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budget Planner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budget Planner
+//
+//  Created by Vishwajeet Sarkar on 06/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
